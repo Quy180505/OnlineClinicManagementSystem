e526a36c0ed3c20b7a59d70b754017a869217f64
LVQ <levanquy180505@gmail.com>
2026-07-25 20:37:24 +0700

    init project

---
+++ online-clinic-management-system/src/test/java/com/ocms/online_clinic_management_system/OnlineClinicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocms.online_clinic_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClinicManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
