b638735677b803d67a9f12c56015baa7475cf7c6
LVQ <levanquy180505@gmail.com>
2026-08-30 23:32:10 +0700

    fix(medicine-category): update category search endpoint

---
+++ online-clinic-management-system/src/main/java/com/ocms/online_clinic_management_system/inventory/dto/request/MedicineCategorySearchRequest.java
@@ -0,0 +1,9 @@
+package com.ocms.online_clinic_management_system.inventory.dto.request;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MedicineCategorySearchRequest {
+    private String categoryName;
+}
